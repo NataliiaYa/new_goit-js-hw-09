676b5e512ce6d61141061d2304c3f4112e45bf0a
NataliiaYa <118573896+NataliiaYa@users.noreply.github.com>
2023-04-23 22:39:44 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,19 +1,20 @@
 
-import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.min.css";
-import { Notify } from "notiflix/build/notiflix-notify-aio";
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import Notiflix from 'notiflix';
 
-const input = document.querySelector("#datetime-picker");
-const startBtn = document.querySelector("[data-start]");
-const days = document.querySelector("[data-days]");
-const hours = document.querySelector("[data-hours]");
-const minutes = document.querySelector("[data-minutes]");
-const seconds = document.querySelector("[data-seconds]");
+const refs = {
+  timerInput: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+  day: document.querySelector('span.value[data-days]'),
+  hour: document.querySelector('span.value[data-hours]'),
+  minute: document.querySelector('span.value[data-minutes]'),
+  second: document.querySelector('span.value[data-seconds]'),
+};
 
-const TIMER_DELAY = 1000;
-startBtn.addEventListener('click', onBtnStart);
-startBtn.disabled = true;
-let intervalId = null;
+refs.btnStart.addEventListener('click', onBtnStart)
+refs.btnStart.disabled = true;
+let futureTime = null;
 
 const options = {
     enableTime: true, //вмикає спосіб вибору часу
@@ -21,66 +22,56 @@ const options = {
     defaultDate: new Date(), //Встановлює початкові вибрані дати.
     minuteIncrement: 1, //Регулює крок для введення хвилин (включно з прокручуванням)
     onClose(selectedDates) { //Функції, які запускаються щоразу, коли календар закривається
-        // console.log(selectedDates[0]);
-    futureTime = selectedDates[0].getTime(); 
-    
+        futureTime = selectedDates[0].getTime();
+
         if (selectedDates[0] <= options.defaultDate) {
-            startBtn.disabled = true; // неактивна кнопка
+            refs.btnStart.disabled = true; // неактивна кнопка
             Notiflix.Notify.failure('Please choose a date in the future');
         } else {
-            startBtn.disabled = false; //активна кнопка
+            refs.btnStart.disabled = false; // активна кнопка
             return futureTime;
         }
     },
 };
 // використовуємо функціонал бібліотеки 
-const dates = flatpickr(input, options);
+const fp = flatpickr(refs.timerInput, options)
 
 function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    const days = addLeadingZero(Math.floor(ms / day));
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
-    return { days, hours, minutes, seconds };
-};
+  return { days, hours, minutes, seconds };
+}
 
 function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
-};
-// функція початку відліку
-// startBtn.addEventListener("click", startTimer);
+    return String(value).padStart(2, '0');
+}
+// функція початку зворотнього відліку
 function onBtnStart() {
-    // const selectedDate = dates.selectedDates[0]
-    // startBtn.disabled = true;
-    // input.disabled = true;
-    intervalId = setInterval(() => {
-        const currentTime = Date.now(); // поточна дата
-        const deltaTime = futureTime - currentTime; //зворотній відлік
-        // const time = convertMs(deltaTime);
-        // startBtn.disabled = true;
-        
-        // updateTimer(time);
+    setInterval(() => {
+        const currentTime = Date.now();
+        const remainingTime = futureTime - currentTime;
 
-        if (deltaTime < 0) {
+        if (remainingTime < 0) {
             return
-        // clearInterval(intervalId);
-
-        // days.textContent = "00";
-        // hours.textContent = "00";
-        // minutes.textContent = "00";
-        // seconds.textContent = "00";
         } else {
-            const {days, hours, minutes, seconds } = convertMs(remainingTime);
-            days.textContent = addLeadingZero(days);
-            hours.textContent = addLeadingZero(hours);
-            minutes.textContent = addLeadingZero(minutes);
-            seconds.textContent = addLeadingZero(seconds);
+            const { days, hours, minutes, seconds } = convertMs(remainingTime);
+            refs.day.textContent = addLeadingZero(days);
+            refs.hour.textContent = addLeadingZero(hours);
+            refs.minute.textContent = addLeadingZero(minutes);
+            refs.second.textContent = addLeadingZero(seconds);
         }
-    }, TIMER_DELAY);
-};
+    }, 1000)
+}
