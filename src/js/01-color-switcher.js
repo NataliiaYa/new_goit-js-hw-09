17d248c5de144948f26f314e1193d08a93bb6d2e
NataliiaYa <118573896+NataliiaYa@users.noreply.github.com>
2023-04-23 19:17:34 +0300

    new

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,39 +2,30 @@
 //Натисканням на кнопку «Stop» зміна кольору фону повинна зупинятися.
 //Для генерування випадкового кольору використовуй функцію getRandomHexColor
 
-const startButton = document.getElementsByTagName('button')[0];
-const stopButton = document.getElementsByTagName('button')[1];
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
 
 let timerId = null;
+let isActive = true;
+startButton.addEventListener('click', startButtonFunc);
+stopButton.addEventListener('click', stopButtonFunc);
 
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
         .toString(16)
         .padStart(6, 0)}`;
 }
-
-// startBtn.addEventListener("click", () => {
-//   timerId = setInterval(() => {
-//     console.log(`I love async JS!  ${Math.random()}`);
-//   }, 1000);
-// });
-startButton.addEventListener('click', startButtonFunc);
+//активуємо кнопку Стар, кнопка Стоп неактивна
 function startButtonFunc() {
+  startButton.disabled = isActive; //кнопка Старт активна
+  stopButton.disabled = !isActive; //кнопка Стоп неактивна
  timerId = setInterval(() => {
  document.body.style.backgroundColor = `${getRandomHexColor()}`;
  }, 1000);
- startButton.disabled = true;
 }
-stopButton.addEventListener('click', stopButtonFunc);
+// активуємо кнопку Стоп, кнопка Старт неактивна, поки працює getRandomHexColor
 function stopButtonFunc() {
   clearInterval(timerId);
-  startButton.disabled = false;
+  stopButton.disabled = isActive; //кнопка Стоп активна
+  startButton.disabled = !isActive; //кнопка Старт неактивна
 }
-
-
-
-
-// stopBtn.addEventListener("click", () => {
-//   clearInterval(timerId);
-//   console.log(`Interval with id ${timerId} has stopped!`);
-// });
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -5,14 +5,16 @@ import { Notify } from "notiflix/build/notiflix-notify-aio";
 
 const input = document.querySelector("#datetime-picker");
 const startBtn = document.querySelector("[data-start]");
-
 const days = document.querySelector("[data-days]");
 const hours = document.querySelector("[data-hours]");
 const minutes = document.querySelector("[data-minutes]");
 const seconds = document.querySelector("[data-seconds]");
 
 const TIMER_DELAY = 1000;
-let intervalId = null
+let intervalId = null;
+startBtn.disabled = true;
+let selectedDate;
+// startBtn.addEventListener("click", startTimer);
 
 const options = {
     enableTime: true, //вмикає спосіб вибору часу
@@ -20,14 +22,10 @@ const options = {
     defaultDate: new Date(), //Встановлює початкові вибрані дати.
     minuteIncrement: 1, //Регулює крок для введення хвилин (включно з прокручуванням)
     onClose(selectedDates) { //Функції, які запускаються щоразу, коли календар закривається
-        const currentDate = new Date(); 
+        console.log(selectedDates[0]);
+        // const currentDate = new Date(); 
     
-        if (selectedDates[0] - currentDate > 0) {
-        startBtn.disabled = false; //активна кнопка
-
-        } else {
-            startBtn.disabled = true; //неактивна кнопка
-
+        if (selectedDates[0] < Date.now()) {
             Notify.warning("Please choose a date in the future",
             {
                 timeout: 5000,
@@ -35,51 +33,64 @@ const options = {
                 fontSize: "25px",
                 position: "center-center"
             },);
+        // startBtn.disabled = false; //активна кнопка
+
+        } else {
+            startBtn.disabled = true; //неактивна кнопка
+            selectedDate = selectedDates[0];
+            // Notify.warning("Please choose a date in the future",
+            // {
+            //     timeout: 5000,
+            //     width: "500px",
+            //     fontSize: "25px",
+            //     position: "center-center"
+            // },);
         }
     },
 };
 // використовуємо функціонал бібліотеки 
 const dates = flatpickr(input, options);
 // функція початку відліку
+startBtn.addEventListener("click", startTimer);
 function startTimer() {
-
-    const selectedDate = dates.selectedDates[0];
-
+    // const selectedDate = dates.selectedDates[0]
+    startBtn.disabled = true;
+    // input.disabled = true;
     intervalId = setInterval(() => {
-        const currentTime = Date.now(); // поточна дата
-        const deltaTime = selectedDate - currentTime; //зворотній відлік
-        const time = convertMs(deltaTime);
-        startBtn.disabled = true;
-
+        const currentTime = selectedDate - Date.now(); // поточна дата
+        // const deltaTime = selectedDate - currentTime; //зворотній відлік
+        // const time = convertMs(deltaTime);
+        // startBtn.disabled = true;
+        
         updateTimer(time);
 
-        if (deltaTime < 0) {
-
+        if (currentTime <= 0) {
         clearInterval(intervalId);
 
-        days.textContent = "00";
-        hours.textContent = "00";
-        minutes.textContent = "00";
-        seconds.textContent = "00";
+        // days.textContent = "00";
+        // hours.textContent = "00";
+        // minutes.textContent = "00";
+        // seconds.textContent = "00";
+        } else {
+            days.textContent = convertMs(difference).days;
+            hours.textContent = convertMs(difference).hours;
+            minutes.textContent = convertMs(difference).minutes;
+            seconds.textContent = convertMs(difference).seconds;
         }
-
     }, TIMER_DELAY);
+};
+
+// function updateTimer(data) {
+//     days.textContent = `${data.days}`;
+//     hours.textContent = `${data.hours}`;
+//     minutes.textContent = `${data.minutes}`;
+//     seconds.textContent = `${data.seconds}`;
+// };
+
+function addLeadingZero(number) {
+  const numberToString = number.toString();
+  return numberToString.padStart(2, '0');
 }
-
-startBtn.addEventListener("click", () => {
-    startTimer();
-});
-
-function updateTimer(data) {
-    days.textContent = `${data.days}`;
-    hours.textContent = `${data.hours}`;
-    minutes.textContent = `${data.minutes}`;
-    seconds.textContent = `${data.seconds}`;
-};
-
-function addLeadingZero(value) {
-    return String(value).padStart(2, "0");
-};
 
 function convertMs(ms) {
     const second = 1000;
